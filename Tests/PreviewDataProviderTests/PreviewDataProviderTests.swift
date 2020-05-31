aa59018a6cb8c491eb0c9dcb269d87cb8705cfff
Ankush Bhatia <ankushbhatia1347@gmail.com>
2020-06-01 00:24:55 +0530

    Removed Sample Project

--- Tests/PreviewDataProviderTests/PreviewDataProviderTests.swift
+++ Tests/PreviewDataProviderTests/PreviewDataProviderTests.swift
@@ -6,7 +6,6 @@ final class PreviewDataProviderTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(PreviewDataProvider().text, "Hello, World!")
     }
 
     static var allTests = [
